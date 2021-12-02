34b832da76f9237da0221ed6cf5feb9a32b5608c
Fabricio Brandan <brandanfabricio7@gmai.com>
2021-12-02 19:23:45 -0300

    repr

--- public/app.js
+++ public/app.js
@@ -8,6 +8,8 @@ async function main(){
     const div = document.getElementById('lista-musica');
     const fragment  = document.createDocumentFragment();
     const template = document.querySelector('#temple').content;
+
+
     data.map(({nombre})=>{
         template.querySelector('#canciones').textContent = nombre;
         const clone = template.cloneNode(true)
@@ -20,6 +22,8 @@ async function main(){
         const canciones = e.textContent;
         e.addEventListener('click',()=>{
             audio.setAttribute('src',`/canciones/${canciones}`)
+            document.getElementById('modal').className = 'ver'
+            document.getElementById('p-cancion').textContent = `${canciones}`
         })
 
 
@@ -27,6 +31,57 @@ async function main(){
 
 
 
+
+     document.querySelector('.icon-lista')
+     .addEventListener('click',()=>{
+       let modal =  document.getElementById('modal');
+       if(modal.className == 'modal '){
+            modal.className = 'ver' 
+       } else{
+           modal.className = 'modal'
+       }   
+     })
+
+
+
+    document.getElementById('play').addEventListener('click', async ()=>{
+        let btn =  document.querySelector('.play')
+        let ruta = btn.src
+        let indice = ruta.includes('/img/play.png')
+
+        const audio = document.getElementById('audio')
+
+            if(indice){
+               btn.src = './img/pausa.png';
+                await audio.play();
+
+            }  else{
+                btn.src = './img/play.png'
+                await    audio.pause();
+            }
+
+        
+
+        
+
+
+    })
+
+
+
+    document.getElementById('agregar-musica')
+    .addEventListener('click',()=>{
+      let modal =  document.querySelector('.ver-mas');
+      if(modal.className == 'ver-mas'){
+           modal.className = 'modal-agragar' 
+      } else{
+          modal.className = 'ver-mas'
+      }   
+    })
+
+    document.getElementById('subir').addEventListener('click',()=>{
+        document.querySelector('.ver-mas').className ='ver-mas'
+    })
 
 
 }
